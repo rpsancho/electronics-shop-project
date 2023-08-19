a2b6d88b302733dad3bf7a8bbb472fa59434a6ca
Alexander Polikarpov <alex-polikarpov@yandex.ru>
2023-08-19 10:56:29 +0700

    modify wrong asserts

--- homework-2/main.py
+++ homework-2/main.py
@@ -12,10 +12,10 @@ if __name__ == '__main__':
     # Exception: Длина наименования товара превышает 10 символов.
 
     Item.instantiate_from_csv()  # создание объектов из данных файла
-    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
+    assert len(Item.all) == 6  # в файле 6 записей с данными по товарам
 
     item1 = Item.all[0]
-    assert item1.name == 'Смартфон'
+    assert item1.name == 'СуперСмарт'
 
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
