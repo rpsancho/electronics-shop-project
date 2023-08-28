19357696f9f3e4f4a065fe48956827db56881a64
Alexander Polikarpov <alex-polikarpov@yandex.ru>
2023-08-28 19:25:46 +0700

    replace class attr __number_of_sim by object attr; fix number_of_sim setter

--- src/phone.py
+++ src/phone.py
@@ -2,15 +2,10 @@ from src.item import Item
 
 
 class Phone(Item):
-    __number_of_sim = 0
 
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
         self.number_of_sim = number_of_sim
-        # if number_of_sim > 0 and isinstance(number_of_sim, int):
-        #     self.__number_of_sim = number_of_sim
-        # else:
-        #     raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля')
 
     @property
     def number_of_sim(self):
@@ -18,7 +13,7 @@ class Phone(Item):
 
     @number_of_sim.setter
     def number_of_sim(self, num):
-        if num > 0 and isinstance(num, int):
+        if isinstance(num, int) and num > 0:
             self.__number_of_sim = num
         else:
             raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля')
